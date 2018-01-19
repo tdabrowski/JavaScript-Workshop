2415eeef8343e49643bee6a6e81ed96340d95b5b
tdabrowski <dabr@poczta.fm>
2018-01-19 12:42:35 +0100

    viewImage function for simple gallery

--- Gallery/js/app.js
+++ Gallery/js/app.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded',function(){
-    
+
+    /**Function for view single image from gallery
+    * @param {string} pictureList selector all li elements with pictures
+    */
+    function viewImage(pictureList){
+        let pictures = document.querySelectorAll(pictureList);
+        let body = document.querySelector('body');
+        pictures.forEach((el,i)=>{
+            el.addEventListener('click',(e)=>{
+                let div = document.createElement('div');
+                let img = document.createElement('img');
+                let button = document.createElement('button');
+                img.setAttribute('src',e.target.getAttribute('src'));
+                div.classList.add('fullScreen');
+                button.classList.add('close');
+                button.innerText = 'Close';
+                div.appendChild(img);
+                div.appendChild(button);
+                body.appendChild(div);
+                button.addEventListener('click',(e)=>{
+                    div.parentElement.removeChild(div);
+                });
+            });
+        });
+    }
+    viewImage('.gallery ul li');
 });
