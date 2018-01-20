50403c8200c946de14ac0a748fc5713e535d438b
tdabrowski <dabr@poczta.fm>
2018-01-20 16:18:46 +0100

    added shoSubMenu function

--- Menu/js/app.js
+++ Menu/js/app.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
-    
-
-
-
-
-
+    /**Showing submenu elements on navigation bar
+    * @param {string} topListSelector Top navigation list (ul) selector
+    */
+    function showSubMenu(topListSelector){
+        const mainMenu = document.querySelector(topListSelector);
+        let menuElements = mainMenu.children;
+        for(let i=0; i<menuElements.length; i++){
+            menuElements[i].addEventListener('mouseover',(e)=>{
+                let subMenu = e.currentTarget.firstElementChild;
+                (subMenu !== null)? subMenu.style.display = 'block': null;
+            });
+            menuElements[i].addEventListener('mouseout',(e)=>{
+                let subMenu = e.currentTarget.firstElementChild;
+                (subMenu !== null)? subMenu.style.display = 'none': null;
+            });
+        }
+    }
+    showSubMenu('.nav ul');
 });
