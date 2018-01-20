474befa94bd5a229592c93ca83373107bf7bf2c9
tdabrowski <dabr@poczta.fm>
2018-01-20 17:58:34 +0100

    tooltip function added

--- Tooltips/js/app.js
+++ Tooltips/js/app.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
-    
+    /**Showing tooltips
+    * @param {string} selector elements with tooltips 
+    */
+    function tooltip(selector){
+        let tooltips = document.querySelectorAll(selector);
+        tooltips.forEach(el=>{
+            el.addEventListener('mouseover',(e)=>{
+                let message = document.createElement('span');
+                message.classList.add('tooltipText');
+                message.innerText = e.currentTarget.dataset.text;
+                e.currentTarget.appendChild(message);
+            });
+            el.addEventListener('mouseout',(e)=>{
+                e.currentTarget.removeChild(e.currentTarget.firstElementChild);
+            });
+        });
+    }
+    tooltip('.tooltip');
 });
