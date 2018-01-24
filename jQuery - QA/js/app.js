77747cd6e8cd218fb450f90a7c4acfba8c019011
tdabrowski <dabr@poczta.fm>
2018-01-24 18:17:51 +0100

    added show hide answer function

--- jQuery - QA/js/app.js
+++ jQuery - QA/js/app.js
@@ -1,1 +1,12 @@
-
+$(function(){
+    /** show-hide answer function
+    * @param {string} selector questions selector
+    */
+    let showAnswer = function(selector){
+        $(selector).on('click',(e)=>{
+            $(selector).next().fadeOut();
+            ($(e.currentTarget).next().css('display') == 'none')?$(e.currentTarget).next().fadeIn():0;
+        });
+    }
+    showAnswer('.QA h1');
+});
