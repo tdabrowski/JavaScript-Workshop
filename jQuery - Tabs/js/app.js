6bb9765ccae001860042bd4f3d04686354ff9ff5
tdabrowski <dabr@poczta.fm>
2018-01-24 19:03:55 +0100

    added function for tabs text view

--- jQuery - Tabs/js/app.js
+++ jQuery - Tabs/js/app.js
@@ -1,1 +1,16 @@
-
+$(function(){
+    /** show-hide text when click on tab
+    * @param {string} selector tabs selector
+    */
+    function tabsHandler(selector) {
+        $(selector).on('click',(e)=>{
+            $('div').each((i,el)=>{
+                if($(el).data('id') == $(e.currentTarget).data('id')){
+                    $(el).fadeToggle();
+                    $(el).toggleClass('selected');
+                }
+            })
+        });
+    }
+    tabsHandler('.tabs li');
+});
